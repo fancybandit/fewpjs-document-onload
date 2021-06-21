6760a12deba68889ef3b30f5743bd710126a044f
Jacob Klimenko <droidedjake@gmail.com>
2021-06-21 15:52:28 -0400

    commit tree 7f68330072b1f3a2e111d1ff7d98fd529a34cc45

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(e) {
+    const p = document.getElementById("text");
+    p.textContent = "This is really cool!";
+});
+
+console.log(
+    "This logs before DOMContentLoaded is triggered, despite being written after the event listener statement"
+);
